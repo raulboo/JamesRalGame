d1d9f580ceff2e6028a1e2a80fefe122a30c2cfc
jamesfoxbr <jamesfoxbr@hotmail.com>
2024-06-06 14:16:20 -0300

    controls prototype

---
+++ src/controls.lua
@@ -0,0 +1,18 @@
+
+-- prototype for  control scheme
+player1 = {}
+
+-- Player 1 controls
+player1.right = "right"
+player1.left  = "left"
+player1.jump  = "space"
+player1.speed = 64
+
+function movePlayer(dt)
+    if love.keyboard.isDown( player1.right ) then 
+        x = x + player1.speed * dt
+    end
+    if love.keyboard.isDown( player1.left ) then 
+        x = x - player1.speed * dt
+    end
+end
