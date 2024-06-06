5f9c927b250871674b4f3cda863fa31cdfee26af
jamesfoxbr <jamesfoxbr@hotmail.com>
2024-06-06 13:14:28 -0300

    organizing folders

--- main.lua
+++ main.lua
@@ -1,13 +1,12 @@
 ---@diagnostic disable: undefined-global
 
 function love.load() 
-    print("aaaaaaa")
+    
 end
 
 function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.rectangle()
-    local a = math.sin(3)
+    
 end
