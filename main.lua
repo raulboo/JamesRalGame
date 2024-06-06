f203d874850609f7928a23f92f000e8345568f7d
jamesfoxbr <jamesfoxbr@hotmail.com>
2024-06-06 14:15:58 -0300

    Organizing folders

--- main.lua
+++ main.lua
@@ -1,18 +1,21 @@
 ---@diagnostic disable: undefined-global
 
 require("src/graphic")
+require("src/controls")
 
 function love.load() 
-    
+    -- variables only for test
+    x = 50
+    y = 50
 end
 
 function love.update(dt)
-    
+    movePlayer(dt)
 end
 
 function love.draw()
     startCanvas({0.5, 0.5, 0.5, 1})
-        love.graphics.rectangle("fill", 100, 100, 50, 50)
+        love.graphics.rectangle("fill", x, y, 50, 50)
     endCanvas()
 end
 
