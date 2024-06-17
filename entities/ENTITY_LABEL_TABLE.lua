d9cbe5f6aff0c450e78d7ae517318a7352cfb5d5
jamesfoxbr <jamesfoxbr@hotmail.com>
2024-06-17 15:12:21 -0300

    a comma was missing in ENTITY_LABEL_TABLE.lua

--- entities/ENTITY_LABEL_TABLE.lua
+++ entities/ENTITY_LABEL_TABLE.lua
@@ -1,7 +1,7 @@
 local FILEPATH = "entities."
 
 return {
-    ["test_player"] = FILEPATH.."001-test_player"
+    ["test_player"] = FILEPATH.."001-test_player",
 
     ["punchbox"]    = FILEPATH.."101-punchbox"
 }
