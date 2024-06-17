08eef9c25ec7d9d0c37aabc5bdba9d5bd1a3c2a2
jamesfoxbr <jamesfoxbr@hotmail.com>
2024-06-17 16:11:30 -0300

    small adjust in the RenderSystem.lua file. Check note

--- systems/RenderSystems.lua
+++ systems/RenderSystems.lua
@@ -5,7 +5,8 @@ local RenderSystems = {}
 
 RenderSystems.renderSprites = system({"pos", "sprite"},
     function(e) 
-        spr(e.sprite, e.pos.x - TILE_SIZE/2, e.pos.y - TILE_SIZE/2)
+        -- IMPORTANT: sprite was not touching ground even if is touching the bottom of the cell i nthe image then I added '+ 1' to e.pos.y to fix
+        spr(e.sprite, e.pos.x - TILE_SIZE/2, e.pos.y - TILE_SIZE/2 + 1) 
     end
 )
 
